e400ee46427194ffb9139adb0a4f7863699be6ee
zouyuxin <yuxin.zou.stat@gmail.com>
2019-02-22 09:36:42 -0600

    add plot for failed run

--- code/plot_susie.R
+++ code/plot_susie.R
@@ -1,5 +1,9 @@
 pdf(plot_file)
 for (r in 1:length(result)) {
-    susie_plot(result[[r]], y='PIP', b=meta$true_coef[,r])
+    if(is.null(result[[r]]$sets)){
+      plot(meta$true_coef[,r], main='Error')
+    }else{
+      susie_plot(result[[r]], y='PIP', b=meta$true_coef[,r])
+    }
 }
 dev.off()
